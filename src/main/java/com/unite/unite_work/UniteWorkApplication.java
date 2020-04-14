9123a4241573de692389c6dc2a4c1c48a1a09837
cyj <1048329675@qq.com>
2020-04-14 19:29:28 +0800

    feat(搭建)：项目的搭建 1.新增pom.xml文件 2.新增src目录

---
+++ src/main/java/com/unite/unite_work/UniteWorkApplication.java
@@ -0,0 +1,13 @@
+package com.unite.unite_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniteWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniteWorkApplication.class, args);
+    }
+
+}
